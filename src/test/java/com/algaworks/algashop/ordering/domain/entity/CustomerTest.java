16c596d5d9435e1df1d36873a9a20938a4062f74
raphaelsegalla <raphael.segalla@gmail.com>
2026-01-19 01:02:07 -0300

    Inicio da implementacao do ordering

---
+++ src/test/java/com/algaworks/algashop/ordering/domain/entity/CustomerTest.java
@@ -0,0 +1,73 @@
+package com.algaworks.algashop.ordering.domain.entity;
+
+import com.algaworks.algashop.ordering.domain.utility.IdGenerator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+
+class CustomerTest {
+
+    @Test
+    void given_invalidEmail_whenTryCreateCustomer_ShouldGenerateException() {
+
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    new Customer(
+                            IdGenerator.generateTimeBasedUUID(),
+                            "John Doe",
+                            LocalDate.of(1991, 7, 5),
+                            "invalid",
+                            "478-256-2504",
+                            "255-08-0578",
+                            false,
+                            OffsetDateTime.now()
+                    );
+                });
+    }
+
+    @Test
+    void given_invalidEmail_whenTryUpdatedCreateCustomerEmail_ShouldGenerateException() {
+        Customer customer = new Customer(
+                IdGenerator.generateTimeBasedUUID(),
+                "John Doe",
+                LocalDate.of(1991, 7, 5),
+                "john.doe@gmail.com",
+                "478-256-2504",
+                "255-08-0578",
+                false,
+                OffsetDateTime.now()
+        );
+
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    customer.changeEmail("invalid");
+                });
+    }
+
+    @Test
+    void given_unarchivedCustomer_whenArchive_ShouldAnonymize() {
+        Customer customer = new Customer(
+                IdGenerator.generateTimeBasedUUID(),
+                "John Doe",
+                LocalDate.of(1991, 7, 5),
+                "john.doe@gmail.com",
+                "478-256-2504",
+                "255-08-0578",
+                false,
+                OffsetDateTime.now()
+        );
+
+        customer.archive();
+
+        Assertions.assertWith(customer,
+                    c -> Assertions.assertThat(c.fullName()).isEqualTo("Anonymous"),
+                    c -> Assertions.assertThat(c.email()).isNotEqualTo("john.doe@gmail.com"),
+                    c -> Assertions.assertThat(c.phone()).isEqualTo("000-000-0000"),
+                    c -> Assertions.assertThat(c.document()).isEqualTo("000-00-0000"),
+                    c -> Assertions.assertThat(c.birthDate()).isNull()
+                );
+    }
+
+}
