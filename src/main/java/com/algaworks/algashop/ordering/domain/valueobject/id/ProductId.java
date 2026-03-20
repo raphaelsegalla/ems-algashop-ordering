ab94328c5d36681dadb2f118f3f464c55710321b
Raphael Segalla <rsegalla@uolinc.com>
2026-03-20 17:02:41 -0300

    6.13. Implementando novos identificadores

--- src/main/java/com/algaworks/algashop/ordering/domain/valueobject/id/ProductId.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/valueobject/id/ProductId.java
@@ -1,18 +1,17 @@
-package com.algaworks.algashop.ordering.domain.valueobject;
+package com.algaworks.algashop.ordering.domain.valueobject.id;
 
 import com.algaworks.algashop.ordering.domain.utility.IdGenerator;
 
 import java.util.Objects;
 import java.util.UUID;
 
-public record CustomerId(UUID value) {
+public record ProductId(UUID value) {
 
-    public CustomerId(UUID value) {
+    public ProductId {
         Objects.requireNonNull(value);
-        this.value = value;
     }
 
-    public CustomerId() {
+    public ProductId() {
         this(IdGenerator.generateTimeBasedUUID());
     }
 
--- src/test/java/com/algaworks/algashop/ordering/domain/valueobject/CustomerIdTest.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/valueobject/CustomerIdTest.java
@@ -1,9 +1,9 @@
 package com.algaworks.algashop.ordering.domain.valueobject;
 
+import com.algaworks.algashop.ordering.domain.valueobject.id.CustomerId;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
 import java.util.UUID;
 
 class CustomerIdTest {
